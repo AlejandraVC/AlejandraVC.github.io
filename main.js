916aef19fb8782aeb1c1ec67a8da203e5ad1c834
AlejandraVC <aleisa.vc@gmail.com>
2019-10-31 17:16:42 -0600

    index.html, style.css & main.js updated
    go-to-top button added

--- main.js
+++ main.js
@@ -24,3 +24,21 @@ function openContactForm() {
     document.body.style.overflowY = "scroll";
   }  
 } 
+
+// GO TO TOP
+ // When the user scrolls down 100px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+     document.getElementById("go-top-button").style.display = "block";
+   } else {
+     document.getElementById("go-top-button").style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function goToTop() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+ }
