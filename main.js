a64fbe066101b20f835139ad1647fe6349696690
AlejandraVC <aleisa.vc@gmail.com>
2019-10-31 16:17:30 -0600

    main.js updated
    overflowY = "hidden"; while Modals are on display

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const projectName = 'portfolio';
 
+window.onload = function () { 
+  document.body.style.overflowY = "hidden";
+}
+    
 function closePopup() {
-    let popupWindow = document.getElementById("popup-window");
-    if (popupWindow.style.display === "none") {
-      popupWindow.style.display = "block";
-    } else {
-      popupWindow.style.display = "none";
-    }
-  } 
+  let popupWindow = document.getElementById("popup-window");
+  if (popupWindow.style.display === "none") {
+    popupWindow.style.display = "block";
+  } else {
+    popupWindow.style.display = "none";
+    document.body.style.overflowY = "scroll";
+  }
+} 
 
-  function openContactForm() {
-    let contactForm = document.getElementById("contact-form");
-    if (contactForm.style.display === "none") {
-        contactForm.style.display = "block";
-      } else {
-        contactForm.style.display = "none";
-    }  
-  } 
+function openContactForm() {
+  let contactForm = document.getElementById("contact-form");
+  if (contactForm.style.display === "none") {
+    contactForm.style.display = "block";
+    document.body.style.overflowY = "hidden";
+  } else {
+    contactForm.style.display = "none";
+    document.body.style.overflowY = "scroll";
+  }  
+} 
