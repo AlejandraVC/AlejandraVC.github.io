6cc077e2bcadbc75d2abe990cfa0f143909a0432
AlejandraVC <aleisa.vc@gmail.com>
2019-11-18 12:46:35 -0600

    Updated index.html and main.js
    index.html:
    - added name="" on input fields on contact form
    main.js: updated Contact Form with formspree API

--- main.js
+++ main.js
@@ -1,76 +1,76 @@
 // Go To Top
 // When the user scrolls down 500px from the top of the document, show the button
 window.onscroll = function() {
-	scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-	if (
-		document.body.scrollTop > 500 ||
-		document.documentElement.scrollTop > 500
-	) {
-		document.getElementById('go-top-button').style.display = 'block';
-	} else {
-		document.getElementById('go-top-button').style.display = 'none';
-	}
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    document.getElementById('go-top-button').style.display = 'block';
+  } else {
+    document.getElementById('go-top-button').style.display = 'none';
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function goToTop() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Open/Close Contact Form Modal
 function openContactForm() {
-	let contactForm = document.getElementById('contact-me-form');
-	if (contactForm.style.display !== 'block') {
-		contactForm.style.display = 'block';
-		document.body.style.overflowY = 'hidden';
-	} else {
-		contactForm.style.display = 'none';
-		document.body.style.overflowY = 'scroll';
-	}
+  let contactForm = document.getElementById('contact-me-form');
+  if (contactForm.style.display !== 'block') {
+    contactForm.style.display = 'block';
+    document.body.style.overflowY = 'hidden';
+  } else {
+    contactForm.style.display = 'none';
+    document.body.style.overflowY = 'scroll';
+  }
 }
 
 //Validate Contact Form content
 function validate() {
-	const name = document.getElementById('contact-name').value;
-	const subject = document.getElementById('contact-subject').value;
-	const email = document.getElementById('contact-email').value;
-	const message = document.getElementById('contact-message').value;
-	let errorMessage = document.getElementById('error_message');
+  const name = document.getElementById('contact-name').value;
+  const subject = document.getElementById('contact-subject').value;
+  const email = document.getElementById('contact-email').value;
+  const message = document.getElementById('contact-message').value;
+  let errorMessage = document.getElementById('error_message');
 
-	errorMessage.style.padding = '10px';
+  errorMessage.style.padding = '10px';
 
-	let text;
+  let text;
 
-	if (name.length === 0) {
-		text = 'PLEASE ENTER A VALID NAME';
-		errorMessage.innerHTML = text;
-		return false;
-	}
+  if (name.length === 0) {
+    text = 'PLEASE ENTER A VALID NAME';
+    errorMessage.innerHTML = text;
+    return false;
+  }
 
-	if (email.indexOf('@') == -1 || email.length < 6) {
-		text = 'PLEASE ENTER A VALID EMAIL';
-		errorMessage.innerHTML = text;
-		return false;
-	}
+  if (email.indexOf('@') == -1 || email.length < 6) {
+    text = 'PLEASE ENTER A VALID EMAIL';
+    errorMessage.innerHTML = text;
+    return false;
+  }
 
-	if (subject.length < 3) {
-		text = 'PLEASE ENTER A VALID SUBJECT';
-		errorMessage.innerHTML = text;
-		return false;
-	}
+  if (subject.length < 3) {
+    text = 'PLEASE ENTER A VALID SUBJECT';
+    errorMessage.innerHTML = text;
+    return false;
+  }
 
-	if (message.length <= 20) {
-		text = 'PLEASE ENTER A VALID MESSAGE';
-		errorMessage.innerHTML = text;
-		return false;
-	}
+  if (message.length <= 20) {
+    text = 'PLEASE ENTER A VALID MESSAGE (MIN 20CHAR)';
+    errorMessage.innerHTML = text;
+    return false;
+  }
 
-	alert('Form Submitted Successfully!');
-	errorMessage.style.display = 'none';
-	openContactForm();
-	return true;
+  // alert('Form Submitted Successfully!');
+  errorMessage.style.display = 'none';
+  openContactForm();
+  return true;
 }
