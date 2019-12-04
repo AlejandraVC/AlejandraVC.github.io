6c399c3728d616ad2e904aceaea41928abae9309
AlejandraVC <aleisa.vc@gmail.com>
2019-12-04 11:16:12 -0600

    Create meme-generator

---
+++ portfolio/meme-generator/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://i.kym-cdn.com/photos/images/facebook/001/234/929/6c9.png"
+        alt="Problem?"
+      />
+      <p>Random Meme Generator</p>
+    </header>
+  )
+}
+export default Header
